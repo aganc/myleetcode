b0df9cd3f91caccbccca37e76117d707e17f9dec
ganzhixin <ganzhixin@anxinsec.com>
2022-09-12 09:02:04 +0800

    hot-100-0912

--- hot-100.go
+++ hot-100.go
@@ -379,3 +379,30 @@ func nextPermutation(nums []int) {
 		j--
 	}
 }
+
+// 32. 最长有效括号
+// https://leetcode.cn/problems/longest-valid-parentheses/
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if target == nums[mid] {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}

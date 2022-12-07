175ed955395877f243442595d7b19ffb2f55f227
mainWj <891277822@qq.com>
2022-12-07 21:28:34 +0800

    [add]236. 二叉树的最近公共祖先
    [add]236. 二叉树的最近公共祖先

--- echo/daily_leecode.go
+++ echo/daily_leecode.go
@@ -120,4 +120,24 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 	return result
 }
 
-
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root.Val == p.Val || root.Val == q.Val{
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left == nil{
+		return right
+	} else if right == nil{
+		return left
+	}
+	return root
+}

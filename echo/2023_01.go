8106b7aad21688c376f4b0a70343d815b96dde7b
mainWj <891277822@qq.com>
2023-01-04 22:01:45 +0800

    [add]151. 反转字符串中的单词

--- echo/2023_01.go
+++ echo/2023_01.go
@@ -28,6 +28,35 @@ func compareVersion(version1 string, version2 string) int {
 	return 0
 }
 
+// 151. 反转字符串中的单词
+func reverseWords(s string) string {
+	lenth := len(s)
+	if lenth <= 1 {
+		return s
+	}
+	res := ""
+	for i:=0; i < lenth ; {
+		word := ""
+		j := i
+		// 读取单词
+		for j < lenth && s[j] != ' '{
+			j ++
+		}
+		word = s[i:j]
+		// 跳过空格
+		for j < lenth && s[j] == ' ' {
+			j ++
+		}
+		// 拼接单词  注意这里
+		if word != "" {
+			res = " " + word + res
+		}
+
+		i = j
+	}
+	return res[1:]
+}
+
 func main(){
 
 	fmt.Print()
